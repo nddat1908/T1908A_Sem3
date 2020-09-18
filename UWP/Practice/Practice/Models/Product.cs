b6eb130b6944b1d0f1097e6edbdbff5ab65673e3
nddat1908 <54591227+nddat1908@users.noreply.github.com>
2020-09-18 15:52:09 +0700

    Practice

---
+++ UWP/Practice/Practice/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+    public class Icon
+    {
+        public string IconPath { get; set; }
+    }
+}
